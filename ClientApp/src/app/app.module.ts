b65799271b0b7d6a72783380498bb3eaec5f18fc
Artur Gawlik <arturgawlik@MacBook-Air-Artur.local>
2019-06-19 12:25:05 +0200

    cleanup

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,37 +1,123 @@
+// -----------------------------------------------------------------------------------------------------  
+// --------------------------------------ng modules-----------------------------------------------------    
+// -----------------------------------------------------------------------------------------------------  
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------       
 
+
+
+
+// -----------------------------------------------------------------------------------------------------  
+// --------------------------------------app components-------------------------------------------------    
+// -----------------------------------------------------------------------------------------------------  
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { ShellComponent } from './components/shell/shell.component';
-import { RoutingModule } from './routing/routing.module';
-import { AngularFireModule } from "@angular/fire";
-import { AngularFireAuthModule } from "@angular/fire/auth";
-import { AuthService } from './services/auth/auth.service';
-import { AuthGuard } from './guards/auth.guard';
-import { LoginGuard } from './guards/login.guard';
-import { FeatherIconsPipe } from './pipes/feather-pipe';
 import { HomeComponent } from './components/home/home.component';
-import { ApplicationHttpClient } from './services/http/applicationHttpClientService.service';
 import { WalletsComponent } from './components/wallets/wallets.component';
 import { AddEditWalletComponent } from './components/add-edit-wallet/add-edit-wallet.component';
-import { AlertModule } from 'ngx-bootstrap/alert';
-import { AddIsValidIsInvalidClassDirective } from './directives/add-is-valid-is-invalid-class.directive';
-import { AddCardRandomColorDirective } from './directives/add-card-random-color.directive';
-import { environment } from '../environments/environment';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { WalletDashboardComponent } from './components/wallet-dashboard/wallet-dashboard.component';
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------         
+
+
+
+
+// --------------------------------------app modules----------------------------------------------------    
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------  
+import { RoutingModule } from './routing/routing.module';
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------         
+
+
+
+
+// -----------------------------------------------------------------------------------------------------  
+// --------------------------------------app services---------------------------------------------------    
+// -----------------------------------------------------------------------------------------------------  
+import { AuthService } from './services/auth/auth.service';
 import { WalletService } from './services/wallet/wallet.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ChartModule } from 'primeng/chart';
-import { AbsolutePipe } from './pipes/absolute.pipe';
-import { ButtonsModule } from 'ngx-bootstrap/buttons';
-import { ScrollingModule } from '@angular/cdk/scrolling';
-import { ToastrModule } from 'ngx-toastr';
+import { ApplicationHttpClient } from './services/http/applicationHttpClientService.service';
 import { MessageService } from './services/message/message.service';
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------         
+
+
+
+
+// -----------------------------------------------------------------------------------------------------  
+// --------------------------------------app pipes------------------------------------------------------    
+// -----------------------------------------------------------------------------------------------------  
+import { AbsolutePipe } from './pipes/absolute.pipe';
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------  
+
+
+
+
+// -----------------------------------------------------------------------------------------------------  
+// --------------------------------------app guards-----------------------------------------------------    
+// -----------------------------------------------------------------------------------------------------  
+import { AuthGuard } from './guards/auth.guard';
+import { LoginGuard } from './guards/login.guard';
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------  
+
+
+
+
+// -----------------------------------------------------------------------------------------------------  
+// --------------------------------------app directives-------------------------------------------------    
+// -----------------------------------------------------------------------------------------------------  
+import { AddIsValidIsInvalidClassDirective } from './directives/add-is-valid-is-invalid-class.directive';
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------  
+
+
+
+
+// -----------------------------------------------------------------------------------------------------  
+// --------------------------------------3rd part modules-----------------------------------------------    
+// -----------------------------------------------------------------------------------------------------  
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { AddCardRandomColorDirective } from './directives/add-card-random-color.directive';
+import { ChartModule } from 'primeng/chart';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { ToastrModule } from 'ngx-toastr';
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------  
+
+
+
+
+// -----------------------------------------------------------------------------------------------------  
+// --------------------------------------settings-------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------      
+import { environment } from '../environments/environment';
+// -----------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------  
+// -----------------------------------------------------------------------------------------------------  
+
+
+
 
 
 @NgModule({
@@ -39,14 +125,12 @@ import { MessageService } from './services/message/message.service';
     AppComponent,
     LoginComponent,
     ShellComponent,
-    FeatherIconsPipe,
     HomeComponent,
     WalletsComponent,
     AddEditWalletComponent,
     AddIsValidIsInvalidClassDirective,
     AddCardRandomColorDirective,
     SignUpComponent,
-    DashboardComponent,
     WalletDashboardComponent,
     AbsolutePipe
   ],
